a48054ff2213b17d9815e1528434216b97fc5b23
Hamid Rza <hamidrza0008@gmail.com>
2026-06-12 20:24:22 +0530

    add sitemap

---
+++ app/site.js
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.undiscoveredstudio.in/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.undiscoveredstudio.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.undiscoveredstudio.in/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.undiscoveredstudio.in/services",
+      lastModified: new Date(),
+    },
+  ];
+}
